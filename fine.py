fe23cf2cde440bd087c9c10e573ae1759a7ab798
Bhuyash.MPGI <65536664+bhuyash@users.noreply.github.com>
2024-08-25 11:52:06 +0530

    Add files via upload

--- fine.py
+++ fine.py
@@ -16,6 +16,16 @@ while ch.lower()=="yes":
     print("You have to pay the fine--1000RS")
   elif att<80 and att>=75:
     print("You have to pay the fine--1500RS + You have to do assignment of each subject")
+  elif att<75 and att>=70:
+    print("You have to pay the fine--2000RS + You have to do 2 assignment of each subject")
+  elif att<70 and att>=65:
+    print("You have to pay the fine--2500RS + You have to do 3 assignment of each subject")
+  elif att<65 and att>=60:
+    print("You have to pay the fine--3000RS + You have to do 4 assignment of each subject")
+  elif att<60 and att>=55:
+    print("You have to pay the fine--3500RS + You have to do 5 assignment of each subject")
+  elif att<55 and att>=50:
+    print("You have to pay the fine--4000RS + You have to do All assignments of each subject")
   else:
-    print("__You are not eligible for test Please Contact Your Coordinator__")
+    print("__Your attendence is less than 50%!!\n You are not eligible for test Please Contact Your Coordinator__")
   ch=input("If you want to check more attendence enter yes: ")
